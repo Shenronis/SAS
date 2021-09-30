2fabbf60e700e9068a33eae4dd216d1b2227c6b6
Shenronis <hoangthien161201@gmail.com>
2021-09-30 19:07:23 +0700

    Update script.js
    Padding WebView

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
 
         if( ios ) {
             if ( !safari ) {
-                $("li").css("margin-bottom", "3em");
+                $("li").css("margin-bottom", "5.5em");
             };
         }
     }
