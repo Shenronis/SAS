ef0a37b0b2ee94e658b726010e0871c36e0ed6ee
Shenronis <hoangthien161201@gmail.com>
2021-10-01 10:20:59 +0700

    Removed jQuery completely

--- script.js
+++ script.js
@@ -1,138 +1,195 @@
-$(document).ready(function () {
-    // $('article').each(function () {
-    //     var i = $(this).attr('id');
-    //     $(`#`+i+` .header`).html(i)
-    // });
+var articles    = null
+var datePicker  = null
+var blankLog    = null
+var contextList = null
+var itemsList    = null
+var heroText    = null
+var title1      = null
+var title2      = null
 
-    // $('.context').each(function() {
-    //     if (isBlank($(this).html()) || $(this).html() == "") {
-    //         $(this).html("<ul><li><none>None</none></li></ul>");
-    //     }
-    // });
+document.addEventListener("DOMContentLoaded", function(event) {
+    articles    = document.getElementsByClassName('articles')
+    blankLog    = document.getElementById('404');
+    datePicker  = document.querySelector('#datePicker')
+    contextList = document.querySelectorAll('.context')
+    itemsList    = document.querySelectorAll('li')
 
-    // $('#datePicker').on('input',function(e){
-    //     search()
-    // });
+    heroText     = document.getElementsByClassName("hero-text")[0];
+    title1       = heroText.querySelector('#hero1');
+    title2       = heroText.querySelector('#hero2');
 
-    // showall()
-
-    // if (isMobile()) {
-    //     $(".hero-text #1").html("[SAS]");
-    //     $(".hero-text #2").html("[SAS]");
-    //     $(".hero-text h2").html("CHANGELOG");
+    //This will add dates to headers with value == ids
+    for (var i = 0, item; item = articles[i]; i++) {
+        var id = item.getAttribute('id');
         
-    //     $(".hero-text #1").css("font-size", "50px");
-    //     $(".hero-text #2").css("font-size", "50px");
+        if (id) {
+            if (id == '404') {break;}
+            var article = item.querySelector('.header');
+            article.innerHTML = id;
+        }
+    }
 
-    //     $(".articles").css("font-size", "100%");
-    //     $(".articles").css("padding", "5%");
-    //     $(".articles .box").css("padding", "10%");
+    //If blank then insert ul>li>none
+    for (var i=0, item; item = contextList[i]; i++) {
+        if (isBlank(item.innerHTML)) {
+            item.innerHTML = "<ul><li><none>None</none></li></ul>";
+        }
+    }
 
-    //     var userAgent = window.navigator.userAgent.toLowerCase(),
-    //     safari = /safari/.test( userAgent ),
-    //     ios = /iphone|ipod|ipad/.test( userAgent );
+    //Add listenter on input[type="date"]
+    datePicker.addEventListener('input', (event) => {
+        var date = event.target.value;
+        search(date);
+    })
 
-    //     if( ios ) {
-    //         if ( !safari ) {
-    //             $("li").css("margin-bottom", "10%");
-    //         }
-    //     }
-    // }
-    // else {
-    //     resize();
-    // }
+    //Add listener on resize event
+    window.addEventListener('resize', function(event) {
+        resize()
+    }, true);
+
+    //Resize elements
+    if (isMobile()) {
+        //Detects iOS Webview
+        var userAgent = window.navigator.userAgent.toLowerCase(),
+        safari = /safari/.test( userAgent ),
+        ios = /iphone|ipod|ipad/.test( userAgent );
+
+        //Resize elem
+        titleHTML        = "[SAS]";
+        fontSize         = 50;
+        title1.innerHTML = titleHTML;
+        title2.innerHTML = titleHTML;
+        title1.style.fontSize = fontSize+'px';
+        title2.style.fontSize = fontSize+'px';
+
+        for (var i = 0, item; item = articles[i]; i++) {
+            var id = item.getAttribute('id');
+            
+            if (id) {
+                var box = item.querySelector('.box');
+                item.style.fontSize = '100%';
+                item.style.padding  = '5%';
+                box.style.padding   = '10%';
+            }
+        }
+
+        if (ios) {
+            if (!safari) {
+                for (var i=0, item; item = itemsList[i]; i++) {
+                    item.style.marginBottom = "10%";
+                }
+            }
+        }
+    } else {
+        resize();
+    }
+    
+    //Show all articles at startup
+    showAll();
+
 });
 
-// $(window).resize(function() {
-//     resize();
-// });
+//Check if using a mobile device/screen
+function isMobile() {
+    var width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+    if (width <= 800) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
+//Resize elements
+function resize() {
+    var width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+    var titleHTML    = null;
+    var fontSize     = null;
 
+    if (width <= 1200) {
+        if (width <= 800) {
+            if (width <= 600) {
+                titleHTML       = "[SAS]";
+                fontSize        = 50;
+            }
+            else {
+                titleHTML       = "StuAgainstSlep";
+                fontSize        = 50;
+            }
+        }
+        else {
+            titleHTML       = "StudentAgainstSleep";
+            fontSize        = 70;
+        }   
+    }
+    else {
+        titleHTML       = "Student Against Sleep";
+        fontSize        = 100;
+    }
+
+    title1.innerHTML      = titleHTML;
+    title2.innerHTML      = titleHTML;
+
+    title1.style.fontSize = fontSize+'px';
+    title2.style.fontSize = fontSize+'px';
+
+}
+
+//Show all articles except blank article 404
+function showAll() {
+    for (var i=0, item; item = articles[i]; i++) {
+        var id = item.getAttribute('id');
+
+        if (id) {
+            if (id != '404') {
+                document.getElementById(id).style.display = "block";
+            }
+            else {
+                document.getElementById(id).style.display = "none";
+            }
+        }
+
+    }
+}
+
+//Search date, filter out articles and show the desired article
+function search(date) {
+    var found = false;
+    //console.log(date)
+    for (var i = 0, item; item = articles[i]; i++) {
+        var id = item.getAttribute('id');
+        //console.log(id);    
+        if (id === date) {
+            document.getElementById(id).style.display = "block";
+            found = true;
+        }
+        else {
+            document.getElementById(id).style.display = "none";
+        }
+    }
+    //If not found insert no changelog with the title as date
+    if (!found) {
+        blankLog.querySelector('.header').innerHTML = date;
+        blankLog.querySelector('.sub-title').innerHTML = "<nolog>[No Changelog]</nolog>";
+        blankLog.style.display = "block";
+    }
+    else {
+        blankLog.style.display = "none";
+    }
+}
+
+
+//Utils
 function isBlank(str) {
     return (!str || /^\s*$/.test(str));
 }
-
 function isMobile() {
     if(window.innerWidth <= 800) {
       return true;
     } else {
       return false;
     }
-}
-
-function resize() {
-    var width = $( window ).width();
-    if (width <= 1200) {
-        if (width <= 800) {
-            $(".hero-text #1").html("StuAgainstSlep");
-            $(".hero-text #2").html("StuAgainstSlep");
-            $(".hero-text h2").html("CHANGELOG");
-            $(".hero-text #1").css("font-size", "50px");
-            $(".hero-text #2").css("font-size", "50px");
-            if (width <= 600) {
-                $(".hero-text #1").html("[SAS]");
-                $(".hero-text #2").html("[SAS]");
-                $(".hero-text h2").html("CHANGELOG");
-                $(".hero-text #1").css("font-size", "50px");
-                $(".hero-text #2").css("font-size", "50px");  
-            }
-        }
-        else {     
-            $(".hero-text #1").html("StudentAgainstSleep");
-            $(".hero-text #2").html("StudentAgainstSleep");
-            $(".hero-text h2").html("CHANGELOG");
-            $(".hero-text #1").css("font-size", "70px");
-            $(".hero-text #2").css("font-size", "70px");     
-        }
-    }
-    else {
-        $(".hero-text #1").html("Student Against Sleep");
-        $(".hero-text #2").html("Student Against Sleep");
-        $(".hero-text h2").html("CHANGELOGGG");
-
-        $(".hero-text #1").css("font-size", "100px");
-        $(".hero-text #2").css("font-size", "100px");
-    }
-}
-
-function search() {
-    var date = document.getElementById("datePicker").value;
-    var found = false;
-
-    $('article').each(function () {
-        var i = $(this).attr('id');
-
-        if (i) {
-            if (i != date) {
-                document.getElementById(i).style.display = "none";
-            }
-            else {
-                document.getElementById(i).style.display = "block";
-                found = true;
-            }
-        }
-    });
-
-    if (!found) {
-        $('#404 .header').html(date);
-        $('#404 .sub-title').html("<nolog>[No Changelog]</nolog>");
-        $('#404').show();
-    }
-    else {
-        $('#404').hide();
-    }
-}
-
-function showall() {
-    $('article').each(function () {
-        var i = $(this).attr('id');
-
-        if (i) {
-            document.getElementById(i).style.display = "block";
-            if (i == '404') {
-                document.getElementById('404').style.display = "none";
-            }
-        }
-    });
 }
